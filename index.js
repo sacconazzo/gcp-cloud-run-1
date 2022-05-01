55990721b69361b72744b0ad1c26c337564bddbb
Giona Righini <grighini@alteanet.it>
2022-05-01 13:32:14 +0200

    test env

--- index.js
+++ index.js
@@ -1,83 +1,82 @@
-const express = require("express");
-const app = express();
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //users
 const users = {
-  test: "test",
-  scn: "try",
-};
+  test: 'test',
+  scn: 'try',
+}
 
 // custom auth
 const auth = (req, res, next) => {
   if (!req.session.user) {
-    res.sendStatus(401);
+    res.sendStatus(401)
   } else {
-    req.session.calls += 1;
-    next();
+    req.session.calls += 1
+    next()
   }
-};
+}
 
-app.get("/", (req, res) => {
-  res.send(`giona.tech API's`);
-});
+app.get('/', (req, res) => {
+  res.send(process.env._WELCOME)
+})
 
-const port = process.env.PORT || 8080;
+const port = process.env._PORT || 8080
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
 
 // sessions
 const store = new MongoDBStore({
-  uri:
-    "mongodb+srv://sacconazzo:Sacconazzo!03@cluster0.ckkuv.gcp.mongodb.net/scn?retryWrites=true&w=majority",
-  databaseName: "scn",
-  collection: "sessions",
+  uri: process.env._DB,
+  databaseName: 'scn',
+  collection: 'sessions',
   connectionOptions: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-});
+})
 app.use(
   session({
-    secret: "my-secret",
+    secret: 'my-secret',
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 60000 * 24 * 60 * 30 },
     store: store,
   })
-);
+)
 
 // main
-app.get("/", (req, res, next) => {
-  res.send("<p>giona.tech API's</p>");
-});
+app.get('/', (req, res, next) => {
+  res.send("<p>giona.tech API's</p>")
+})
 
-app.post("/login", (req, res, next) => {
+app.post('/login', (req, res, next) => {
   if (!!users[req.body.user] && users[req.body.user] === req.body.password) {
     //req.session.regenerate(() => {
-    req.session.user = req.body.user;
-    if (!req.session.calls) req.session.calls = 0;
-    res.sendStatus(200);
+    req.session.user = req.body.user
+    if (!req.session.calls) req.session.calls = 0
+    res.sendStatus(200)
     //});
   } else {
-    req.session.destroy();
-    res.sendStatus(401);
+    req.session.destroy()
+    res.sendStatus(401)
   }
-});
+})
 
-app.get("/session", auth, (req, res, next) => {
-  res.json(req.session);
-});
+app.get('/session', auth, (req, res, next) => {
+  res.json(req.session)
+})
 
-app.post("/data", auth, (req, res, next) => {
-  req.session.data = req.body;
-  res.sendStatus(201);
-});
+app.post('/data', auth, (req, res, next) => {
+  req.session.data = req.body
+  res.sendStatus(201)
+})
 
 // Exports for testing purposes.
-module.exports = app;
+module.exports = app
